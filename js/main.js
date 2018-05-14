8377b216bc4ea9fb707f554ecf7b6336dcb7c222
SHWETA KULKARNI <kulsv1393@gmail.com>
2018-05-13 20:13:36 -0400

    Final code changes before testing

--- js/main.js
+++ js/main.js
@@ -1,50 +1,117 @@
 // User code here
 
 // HINT: start here: https://swapi.co/api/films/
-/*
-$(document).ready(function(){
-	alert('in client js!!' );
-	var data = {};
-	data.title = "title";
-	data.message = "message";
-	$.ajax({
-		type: 'POST',
-		data: JSON.stringify(data),
-		contentType: 'application/json',
-		url: 'http://127.0.0.1:8080/js/test/main-test',
-		success: function (data) {
-			console.log('success');
-			console.log(JSON.stringify(data));
-		}
-	});
-			
-			
-});*/
-
 
 $(document).ready(function() {
+	var removeArray = [];
+	var new_result = '';
+	var dict = new Object();
 	
-	$( "#swFilms" ).click(function() {
-	  alert( "Handler for .click() called." );
-			
+	// displaying gilms on click of dropdown
+	$(document).on('click', '#swFilms', function() {
+		swurl = 'https://swapi.co/api/films/';
 		$.ajax({
-			url: 'http://127.0.0.1:8080/',
-			//dataType: "jsonp",
+			url: swurl,
 			dataType: "json",
-			data: '{"data": "TEST"}',
-			type: 'POST',
-			//jsonpCallback: 'callback', // this is not relevant to the POST anymore
+			type: 'GET',
 			success: function (data) {
-				var ret = JSON.parse(data);
-				//$('#lblResponse').html(ret.msg);
-				console.log('Success: ' + ret)
+				var ret = data['results'];
+				new_result = ret.sort( sortBy("release_date") );
+				var moviesArray = [];
+				for(var i = 0; i < new_result.length; i++) {
+					var jsonData = {};
+					var obj = new_result[i];
+					jsonData['id'] = i;
+					jsonData['name'] = obj['title'];
+					moviesArray.push(jsonData);
+				}
+				dropdown = $('.dropdown-menu');
+				dropdown.empty();
+				dropdown.prop('selectedIndex', 0);
+				var data = '';
+				$.each(moviesArray, function (key, entry) {
+					data += '<li class="dd_option" id=' + entry['id'] + '><a href="#">' + entry['name'] + '</a></li>';
+			  })
+				dropdown.append(data);
 			},
-			error: function (xhr, status, error) {
+			error: function (error) {
 				console.log('Error: ');
-				//$('#lblResponse').html('Error connecting to the server.');
 			},
 		});
 	});
-	//console.log('in client js!');
-   
-});
+	
+	//displaying characters and spaceships on click of film name
+	$(document).on('click', '.dd_option', function() {
+		$('.char-table').empty();
+		$('#swFilms').text($(this).text());
+		$('#swFilms').append('  <span class="caret"></span>');
+		var id = this.id;
+		c_data = new_result[id];
+		var characters = c_data['characters'];
+		$.each(characters, function (i, item) {
+			var name = '';
+			var sship_url = [];
+			var c_url = item;
+			$.ajax({
+				url: c_url,
+				dataType: "json",
+				type: 'GET',
+				success: function (data) {
+					name = data['name'];
+					sship_url = data['starships'];
+					var sname_array = [];
+					if (!(name in dict)){
+
+						$.each(sship_url, function (j, sship) {
+							var s_name = '';
+							$.ajax({
+								url: sship,
+								dataType: "json",
+								type: 'GET',
+								success: function (s_data) {
+									s_name = s_data['name'];
+									sname_array.push(s_name);
+								},
+								async: false
+							});
+						
+						});
+						dict[name] = sname_array;
+					}
+					if(!removeArray.includes(name)){
+						displayList = $('.char-table');
+						disp = '<tr><td class = ' + name +'>' + name + '<a href="#"><span class="glyphicon glyphicon-remove " id=' + name +'></span></a></td>';
+						if(dict[name].length > 0)
+							disp += '<td>' + dict[name] + '</td></tr>';
+						else
+							disp += '<td>' + 'No Starships' + '</td></tr>';
+						displayList.append(disp);
+					}
+				},
+				//async: false
+			});
+		});		
+	});
+
+	//removing character record from dsplayed list on click of red cross
+	$(document).on('click', '.glyphicon-remove', function() {
+		var id = this.id;
+		var cls = '.'+id;
+		var name = $(cls).text();
+		removeArray.push(name);
+		alert('This character will be removed from the future results!');
+		$(this).closest('tr').remove();
+	});
+	
+});//end of document
+
+function sortBy(val){
+   return function(a,b){
+      if( a[val] > b[val]){
+          return 1;
+      }else if( a[val] < b[val] ){
+          return -1;
+      }
+      return 0;
+   }
+}
--- js/test/main-test.js
+++ js/test/main-test.js
@@ -29,98 +29,9 @@ http.createServer(function (request, response) {
         
         else {
 		    response.writeHead(200, { 'Content-Type': contentType });
-			//response.send('hello!!');
-			//messageHandler(request, response);
-			//response.write('callback(\'{\"msg\": \"OK\"}\')');
-        	response.write(content, 'utf-8');
+			response.write(content, 'utf-8');
 			response.end();
         }
     });	
-	
-	request.on('data', function (chunk) {
-		//response.write('hi');
-        console.log('GOT DATA!' + chunk);
-		
-		var result = [];
-		var url = 'https://swapi.co/api/films/';
-		var https = require('https');
-		https.get(url, function(res){
-			var body = '';
-			res.on('data', function(chunk){
-				body += chunk;
-			});
-			res.on('end', function(){
-				var resp = JSON.parse(body);
-				result = resp['results'];
-				//console.log(resp['results']);
-				var new_result = result.sort( sortBy("release_date") );
-				var moviesArray = [];
-				for(var i = 0; i < new_result.length; i++) {
-					var jsonData = {};
-					var obj = new_result[i];
-					jsonData['id'] = obj['episode_id'];
-					jsonData['name'] = obj['title'];
-					moviesArray.push(jsonData);
-				}
-				//s.end(moviesArray);
-				/*for(var i = 0; i < movies.length; i++) {
-					
-					var obj = movies[i];
-					console.log('id :: ' + obj['id']);
-					console.log('value :: ' + obj['name']);
-					
-				}*/
-				
-			});
-		}).on('error', function(e){
-			  console.log("Got an error: ", e);
-			});	
-    });
-/*******
-	var result = [];
-	var url = 'https://swapi.co/api/films/';
-	var https = require('https');
-	https.get(url, function(res){
-		var body = '';
-		res.on('data', function(chunk){
-			body += chunk;
-		});
-		res.on('end', function(){
-			var resp = JSON.parse(body);
-			result = resp['results'];
-			//console.log(resp['results']);
-			var new_result = result.sort( sortBy("release_date") );
-			var moviesArray = [];
-			for(var i = 0; i < new_result.length; i++) {
-				var jsonData = {};
-				var obj = new_result[i];
-				jsonData['id'] = obj['episode_id'];
-				jsonData['name'] = obj['title'];
-				moviesArray.push(jsonData);
-			}
-			/*for(var i = 0; i < movies.length; i++) {
-				
-				var obj = movies[i];
-				console.log('id :: ' + obj['id']);
-				console.log('value :: ' + obj['name']);
-				
-			}
-			
-		});
-	}).on('error', function(e){
-		  console.log("Got an error: ", e);
-		});
-*******/
 	 
 }).listen(8080);
-
-function sortBy(prop){
-   return function(a,b){
-      if( a[prop] > b[prop]){
-          return 1;
-      }else if( a[prop] < b[prop] ){
-          return -1;
-      }
-      return 0;
-   }
-}
